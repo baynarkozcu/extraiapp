269e5dd6e2498a5fe8e0037240e213d7b4e4f89a
baynarkozcu <enesozudogruu@gmail.com>
2021-04-30 15:01:32 +0300

    question widget is created

---
+++ lib/question.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String questionText;
+
+  Question(this.questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        child: Text(
+          questionText,
+          textAlign: TextAlign.center ,
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ));
+  }
+}
